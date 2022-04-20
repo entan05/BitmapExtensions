d3daaa07841b12ec1054741b052035087b30cf17
entan05 <>
2022-04-20 23:12:08 +0900

    minSdkの変更とbuild.gradleの修正

--- app/src/main/java/jp/team/ework/bitmapextensions/sample/MainActivity.kt
+++ app/src/main/java/jp/team/ework/bitmapextensions/sample/MainActivity.kt
@@ -1,18 +1,20 @@
 package jp.team.ework.bitmapextensions.sample
 
 import android.graphics.drawable.BitmapDrawable
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
 import jp.team.ework.bitmapextensions.mosaic.averagePooling
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         findViewById<ImageView>(R.id.processed_image).let { imageView ->
-            val bitmap = (getDrawable(R.drawable.lenna) as BitmapDrawable).bitmap
+            val bitmap =
+                (AppCompatResources.getDrawable(this, R.drawable.lenna) as BitmapDrawable).bitmap
             imageView.setImageBitmap(bitmap.averagePooling())
         }
     }
